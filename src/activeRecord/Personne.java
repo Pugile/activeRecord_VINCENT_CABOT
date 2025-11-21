5c6a4da16f7c8a7646d1631f895418dd1ac9d6b2
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-11-21 15:43:09 +0100

    Ajout de la classe Personne.java

---
+++ src/activeRecord/Personne.java
@@ -0,0 +1,14 @@
+package activeRecord;
+
+public class Personne {
+
+    int id;
+    String nom;
+    String prenom;
+
+    public Personne( String nom, String prenom) {
+        this.id = -1;
+        this.nom = nom;
+        this.prenom = prenom;
+    }
+}
