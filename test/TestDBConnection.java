8e5aab51e25674fe5a97554fdedf82e7cc948303
Matthieu Cabot <matthieu.cabot3@etu.univ-lorraine.fr>
2025-11-27 10:47:07 +0100

    Ajout de la javadoc pour les classes de test

--- test/TestDBConnection.java
+++ test/TestDBConnection.java
@@ -7,6 +7,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestDBConnection {
 
+    /**
+     * Test qui permet de vérifier qu'il y a toujours 1 seule instance de Connection.
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @Test
     public void testSingleton() throws SQLException, ClassNotFoundException {
 
@@ -17,11 +22,16 @@ public class TestDBConnection {
         assertNotNull(c1);
     }
 
+    /**
+     * Test la méthode setNomDB qui change le nom de la base de données et crée une nouvelle connexion
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @Test
     public void testChangementBase() throws SQLException, ClassNotFoundException {
 
         Connection c1 = DBConnection.getConnection();
-        DBConnection.setNomDB("testpersonne2"); //J'ai créer une deuxième base de donnée pour tester cette méthode
+        DBConnection.setNomDB("testpersonne2"); //J'ai créer une deuxième base de données pour tester cette méthode
         Connection c2 = DBConnection.getConnection();
 
         assertNotSame(c1, c2, "Après changement de nom de DB, l'instance devrait être nouvelle");
--- test/TestPersonne.java
+++ test/TestPersonne.java
@@ -8,12 +8,23 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test les méthodes de la classe Personne
+ */
 public class TestPersonne {
 
+    /**
+     * Ce lance avant chaque test et permet de construire la table personne
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @BeforeEach
     public void setUp() throws SQLException, ClassNotFoundException {
+        //On supprime la table existante pour partir sur une base nouvelle.
         Personne.deleteTable();
         Personne.createTable();
+
+        //On ajoute les objets Personnes
         Personne p1 = new Personne("Spielberg", "Steven");
         p1.save();
 
@@ -27,38 +38,65 @@ public class TestPersonne {
         p4.save();
     }
 
+    /**
+     * Supprime après chaque test la table personne
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @AfterEach
     public void supprTable() throws SQLException, ClassNotFoundException {
         Personne.deleteTable();
     }
 
+    /**
+     * Test de la méthode testFindAll qui retourne tous les lignes de la table personne
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @Test
     public void testFindAll() throws SQLException, ClassNotFoundException {
         ArrayList<Personne> liste = Personne.findAll();
 
+        //On test que la taille de la liste est correct
         assertEquals(4, liste.size(), "La table devrait contenir exactement 4 personnes");
 
+        //On test si la première personne de la liste est bien la première qui est présente dans la base
         Personne p1 = liste.get(0);
         assertEquals(1, p1.getId());
         assertEquals("Spielberg", p1.getNom());
     }
 
 
+    /**
+     * Test la méthode FindById qui retourne une personne suivant l'id passé en paramètre
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @Test
     public void testFindById() throws SQLException, ClassNotFoundException {
+
+        //On test que la personne qui a l'id 1 est la bonne
         Personne p1 = Personne.findById(1);
         assertNotNull(p1, "L'id 1 doit exister");
         assertEquals("Spielberg", p1.getNom());
         assertEquals("Steven", p1.getPrenom());
 
+        //On test que la personne qui a l'id 3 est la bonne
         Personne p3 = Personne.findById(3);
         assertNotNull(p3, "L'id 3 doit exister");
         assertEquals("Kubrick", p3.getNom());
+        assertEquals("Stanley", p3.getPrenom());
 
+        //On vérifie que la méthode renvoie bien null s'il n'y a pas d'utilisateur à cette id
         Personne pInconnu = Personne.findById(10);
         assertNull(pInconnu, "L'id 10 ne doit pas exister");
     }
 
+    /**
+     * Test la méthode FindByName qui retourne une personne suivant le nom passer en paramètre
+     * @throws SQLException
+     * @throws ClassNotFoundException
+     */
     @Test
     public void testFindByName() throws SQLException, ClassNotFoundException {
 
@@ -67,6 +105,7 @@ public class TestPersonne {
         assertEquals("Ridley", listeScott.get(0).getPrenom());
         assertEquals(2, listeScott.get(0).getId());
 
+        //On vérifie que la méthode renvoie bien une liste vide si le nom n'existe pas
         ArrayList<Personne> listeInconnue = Personne.findByName("Tarantino");
         assertTrue(listeInconnue.isEmpty(), "La liste devrait être vide pour un nom inconnu");
     }
